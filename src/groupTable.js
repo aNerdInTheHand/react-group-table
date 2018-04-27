6eead5d9910fdbd60cc18bc2940bc729d5113221
Nick Holvast <nicholas.holvast@dwp.gsi.gov.uk>
2018-04-27 18:31:20 +0100

    class -> className moving wireframe to React component

--- src/groupTable.js
+++ src/groupTable.js
@@ -4,68 +4,72 @@ import propTypes, { defaultProps } from './propTypes/groupTable'
 export default class GroupTable extends Component {
   render () {
     return (
-      <div class='table-wrapper'>
-        <table class='group-table'>
-          <tr class='group-table-header-row'>
-            <th class='empty-th' />
-            <th class='empty-th' />
-            <th>P</th>
-            <th>W</th>
-            <th>D</th>
-            <th>L</th>
-            <th>GD</th>
-            <th>GS</th>
-            <th>GC</th>
-            <th class='th-em'>PTS</th>
-          </tr>
-          <tr class='group-table-content-row group-qualifying-position'>
-            <td class='qual-pos'>1</td>
-            <td class='td-em group-table-team-name'>Romania</td>
-            <td>3</td>
-            <td>2</td>
-            <td>1</td>
-            <td>0</td>
-            <td>2</td>
-            <td>4</td>
-            <td>7</td>
-            <td class='th-em'>2</td>
-          </tr>
-          <tr class='group-table-content-row group-qualifying-position last-qualifying-pos'>
-            <td class='qual-pos'>2</td>
-            <td class='td-em group-table-team-name'>England</td>
-            <td>3</td>
-            <td>2</td>
-            <td>0</td>
-            <td>1</td>
-            <td>3</td>
-            <td>5</td>
-            <td>2</td>
-            <td class='th-em'>2</td>
-          </tr>
-          <tr class='group-table-content-row group-no-qualifying-position'>
-            <td class='no-qual-pos'>3</td>
-            <td class='td-em group-table-team-name'>Colombia</td>
-            <td>3</td>
-            <td>1</td>
-            <td>0</td>
-            <td>2</td>
-            <td>-2</td>
-            <td>1</td>
-            <td>3</td>
-            <td class='th-em'>3</td>
-          </tr>
-          <tr class='group-table-content-row group-no-qualifying-position'>
-            <td class='no-qual-pos'>4</td>
-            <td class='td-em group-table-team-name'>Tunisia</td>
-            <td>3</td>
-            <td>0</td>
-            <td>1</td>
-            <td>2</td>
-            <td>-3</td>
-            <td>1</td>
-            <td>4</td>
-            <td class='th-em'>1</td>
-          </tr>
+      <div className='table-wrapper'>
+        <table className='group-table'>
+          <thead>
+            <tr className='group-table-header-row'>
+              <th className='empty-th' />
+              <th className='empty-th' />
+              <th>P</th>
+              <th>W</th>
+              <th>D</th>
+              <th>L</th>
+              <th>GD</th>
+              <th>GS</th>
+              <th>GC</th>
+              <th className='th-em'>PTS</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr className='group-table-content-row group-qualifying-position'>
+              <td className='qual-pos'>1</td>
+              <td className='td-em group-table-team-name'>Romania</td>
+              <td>3</td>
+              <td>2</td>
+              <td>1</td>
+              <td>0</td>
+              <td>2</td>
+              <td>4</td>
+              <td>7</td>
+              <td className='th-em'>2</td>
+            </tr>
+            <tr className='group-table-content-row group-qualifying-position last-qualifying-pos'>
+              <td className='qual-pos'>2</td>
+              <td className='td-em group-table-team-name'>England</td>
+              <td>3</td>
+              <td>2</td>
+              <td>0</td>
+              <td>1</td>
+              <td>3</td>
+              <td>5</td>
+              <td>2</td>
+              <td className='th-em'>2</td>
+            </tr>
+            <tr className='group-table-content-row group-no-qualifying-position'>
+              <td className='no-qual-pos'>3</td>
+              <td className='td-em group-table-team-name'>Colombia</td>
+              <td>3</td>
+              <td>1</td>
+              <td>0</td>
+              <td>2</td>
+              <td>-2</td>
+              <td>1</td>
+              <td>3</td>
+              <td className='th-em'>3</td>
+            </tr>
+            <tr className='group-table-content-row group-no-qualifying-position'>
+              <td className='no-qual-pos'>4</td>
+              <td className='td-em group-table-team-name'>Tunisia</td>
+              <td>3</td>
+              <td>0</td>
+              <td>1</td>
+              <td>2</td>
+              <td>-3</td>
+              <td>1</td>
+              <td>4</td>
+              <td className='th-em'>1</td>
+            </tr>
+          </tbody>
         </table>
       </div>
     )
